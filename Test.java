3b97343a53f60313ff265c67796cc56dd986199b
Aryan Kalyansingh Thakur <aryankthakur2004@gmail.com>
2026-03-05 14:19:23 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(String[] a){
         System.out.println("Hello World!");
-        System.out.println("Aryan this side");
+        System.out.println("Aryan this side and Vishal that side");
     }
 }
