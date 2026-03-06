a2cceb474b7fcbbfa7ea0980836ff3e4a93af6de
Aryan Kalyansingh Thakur <aryankthakur2004@gmail.com>
2026-03-06 14:06:21 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(String[] a){
         System.out.println("Hello World!");
-        System.out.println("This is an example of Hook");
+        System.out.println("This is an example of Hook with pipeline");
     }
 }
