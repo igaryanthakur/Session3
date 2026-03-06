07a264a4692597fad590ce325ea7d124685b5236
Aryan Kalyansingh Thakur <aryankthakur2004@gmail.com>
2026-03-06 13:30:21 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(String[] a){
         System.out.println("Hello World!");
-        System.out.println("Aryan this side and Vishal that side");
+        System.out.println("This is an example of Hook");
     }
 }
